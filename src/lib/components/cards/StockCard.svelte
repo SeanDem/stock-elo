4a1488bbfa69b97c666e6797fb215b44e21c7ae0
Sean Demeritt <seandemeritt13@gmail.com>
2025-01-12 00:28:57 -0500

    update dependencies and fix prettier

--- src/lib/components/cards/StockCard.svelte
+++ src/lib/components/cards/StockCard.svelte
@@ -3,7 +3,7 @@
 	import type { StockCardProps } from '$lib/components/cards/stock-card.types';
 	import { stockCardSkeleton } from '$lib/components/cards/skeleton';
 
-	export let stock: StockCardProps = stockCardSkeleton
+	export let stock: StockCardProps = stockCardSkeleton;
 	export let onClick: (symbol: string) => void;
 
 	function getChangeClass(change: number | string): string {
@@ -25,11 +25,7 @@
 >
 	<div class="card-body gap-y-4">
 		<div class="flex items-start h-28">
-			<img
-				alt={stock.ticker}
-				class="h-28 w-auto max-w-32 object-contain"
-				src={stock.logoUrl}
-			/>
+			<img alt={stock.ticker} class="h-28 w-auto max-w-32 object-contain" src={stock.logoUrl} />
 			<div class="w-2/3 ml-4 overflow-hidden">
 				<h2 class="card-title text-xl font-semibold truncate">
 					{stock.ticker}
@@ -38,24 +34,29 @@
 					{stock.name}
 				</p>
 			</div>
-	</div>
+		</div>
 		<div class="flex justify-between">
 			<div class="flex flex-col">
 				<p class="text-base-content">
-					<span class="font-semibold">Price:</span> { Format.currency(stock.price) }
+					<span class="font-semibold">Price:</span>
+					{Format.currency(stock.price)}
 				</p>
 				<p class="text-base-content">
-					<span class="font-semibold">Market Cap:</span> { Format.marketCap(stock.marketCap) }
+					<span class="font-semibold">Market Cap:</span>
+					{Format.marketCap(stock.marketCap)}
 				</p>
 				{#if stock.volume}
 					<p class="text-base-content">
-						<span class="font-semibold">Volume:</span> { Format.currency(stock.volume) }
+						<span class="font-semibold">Volume:</span>
+						{Format.currency(stock.volume)}
 					</p>
 				{/if}
 				{#if stock.todaysChangePerc}
 					<p class="text-base-content">
 						<span class="font-semibold">Change (24h):</span>
-						<span class={getChangeClass(stock.todaysChangePerc)}> { Format.percentage(stock.todaysChangePerc) }</span>
+						<span class={getChangeClass(stock.todaysChangePerc)}>
+							{Format.percentage(stock.todaysChangePerc)}</span
+						>
 					</p>
 				{/if}
 			</div>
